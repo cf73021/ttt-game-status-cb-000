62fb43bc2521d2faaee76ecdbe099b11102bdec3
Chris Franklin <cf73021@gmail.com>
2018-07-05 14:44:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -37,10 +37,8 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true
-  elsif !won?(board) && !full?(board)
-    return false
   else
-    false
+    return false
   end
 end
 
